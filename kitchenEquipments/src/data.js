38e72acd6dc14326428b153a6323aa0795ea94ef
akmaljon1946 <akmaljon.ahmedjanov@yandex.ru>
2024-07-21 08:49:12 -0500

    gn11

--- kitchenEquipments/src/data.js
+++ kitchenEquipments/src/data.js
@@ -1,17 +1,33 @@
 export const card = [
 	{
-		image: 'GN1.jpg',
+		image: 'GN20.jpg',
 		title:
-			'GN1/1',
+			'GN1/1/20',
 	},
 	{
-		image: 'GN1.jpg',
+		image: 'GN40.jpg',
 		title:
-			'GN1/1',
+			'GN1/1/40',
 	},
 	{
-		image: 'GN1.jpg',
+		image: 'GN65.jpg',
 		title:
-			'GN1/1',
+			'GN1/1/65',
 	},
+	{
+		image: 'GN100.jpg',
+		title:
+			'GN1/1/100',
+	},
+	{
+		image: 'GN150.jpg',
+		title:
+			'GN1/1/150',
+	},
+	{
+		image: 'GN200.jpg',
+		title:
+			'GN1/1/200',
+	},
+
 ]
