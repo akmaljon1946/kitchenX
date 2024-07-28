015a7cee7e5597776076033866f991ae03871a7a
akmaljon1946 <akmaljon.ahmedjanov@yandex.ru>
2024-07-28 17:39:24 +0500

    link

--- kitchenEquipments/src/components/Home/Home.jsx
+++ kitchenEquipments/src/components/Home/Home.jsx
@@ -7,8 +7,8 @@ const  Home = () => (
 	<>
 		<div className='header'>
 			<h1 className='header-title'>KitchenXequipments</h1>
-				<Link className='link' to="/information">Гастроемкости</Link>
-				<Link className='link' to="/about">Нейтральное оборудования</Link>
+				<Link className='link fromRight' to="/information">Гастроемкости</Link>
+				<Link className='link fromRight' to="/about">Нейтральное оборудования</Link>
 		</div>			
 		<div className="main">
 			<img src="/kitchen.jpg" alt="kitchen" className="main-img" />
