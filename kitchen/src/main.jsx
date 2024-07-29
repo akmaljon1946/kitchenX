a9bc9286367b3c1917c8547a9d3d724835c0c47c
akmaljon1946 <akmaljon.ahmedjanov@yandex.ru>
2024-07-29 17:27:43 +0500

    hash

--- kitchen/src/main.jsx
+++ kitchen/src/main.jsx
@@ -1,10 +1,14 @@
-
+import{HashRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
+  const root= ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    // eslint-disable-next-line react/jsx-no-undef
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
