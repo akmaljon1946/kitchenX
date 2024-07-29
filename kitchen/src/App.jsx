4c780c67f99634fe22b69bd3a76113ffbc7c8e55
akmaljon1946 <akmaljon.ahmedjanov@yandex.ru>
2024-07-29 17:32:58 +0500

    rout

--- kitchen/src/App.jsx
+++ kitchen/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Information from './components/Information/Information';
 import Home from './components/Home/Home';
 import About from './components/About/About';
